a2592f74960a225395f7a914098d62fe523c8386
Kevins Bobo <kevins.bobo@gmail.com>
2016-11-23 23:21:14 +0800

    add 07_StackQueue_07_getMaxWindows.c

---
+++ zuocodebook/01_StackQueue_07_getMaxWindows.c
@@ -0,0 +1,145 @@
+/*
+ * 打印窗口最大值
+ * */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef int Elemtype;
+
+// 双队列队列结点定义
+typedef struct Node{
+	Elemtype data;
+	struct Node *next;
+	struct Node *pre;
+} Node;
+
+// 双队列顶点定义
+typedef struct{
+	Node *top;
+	Node *tail;
+	int  count;
+} Dueuetop;
+
+// 初始化双队列
+void init(Dueuetop *Dueue){
+	Dueue->top = NULL;
+	Dueue->tail = NULL;
+	Dueue->count = 0;
+}
+
+// 基本push功能
+void dueuePush(Dueuetop *Dueue, Elemtype e){
+	Node *s = malloc(sizeof(Node));
+	s->data = e;
+	s->next = Dueue->top;
+	s->pre  = NULL;
+	if(Dueue->top != NULL)
+		Dueue->top->pre = s;
+	Dueue->top = s;
+	if(Dueue->tail == NULL)
+		Dueue->tail = s;
+	Dueue->count += 1;
+}
+
+// 基本pushtail功能
+void dueuePushTail(Dueuetop *Dueue, Elemtype e){
+	Node *s = malloc(sizeof(Node));
+	s->data = e;
+	s->next = NULL;
+	s->pre  = Dueue->tail;
+	if(Dueue->tail != NULL)
+		Dueue->tail->next = s;
+	Dueue->tail = s;
+	if(Dueue->top == NULL)
+		Dueue->top = s;
+	Dueue->count += 1;
+}
+
+// 基本pop功能
+Elemtype dueuePop(Dueuetop *Dueue){
+	if(Dueue->top == NULL)
+		return 0;
+	Node *p;
+	Elemtype e;
+	e = Dueue->top->data;
+	p  = Dueue->top;
+	Dueue->top = p->next;
+
+	if(Dueue->top == NULL)
+		Dueue->tail = NULL;
+	else
+		p->next->pre = NULL;
+
+	Dueue->count -= 1;
+	free(p);
+	return e;
+}
+
+// 基本poptail功能
+Elemtype dueuePopTail(Dueuetop *Dueue){
+	if(Dueue->tail == NULL)
+		return 0;
+	Node *p;
+	Elemtype e;
+	e = Dueue->tail->data;
+	p  = Dueue->tail;
+	Dueue->tail = p->pre;
+
+	if(Dueue->tail == NULL)
+		Dueue->top = NULL;
+	else
+		p->pre->next = NULL;
+
+	Dueue->count -= 1;
+	free(p);
+	return e;
+}
+
+void getMaxWindows(int num, int const *buffer, int window){
+	Dueuetop Dueue;
+	init(&Dueue);
+	int i;
+	int j;
+
+	printf("{");
+
+	for(i = 0;i < num; i ++){
+		while(Dueue.tail != NULL && buffer[Dueue.tail->data] <= buffer[i])
+			dueuePopTail(&Dueue);
+		
+		dueuePushTail(&Dueue, i);
+
+		if(Dueue.top->data == i - window)
+			dueuePop(&Dueue);
+
+		if(i >= window - 1){
+			printf("%d", buffer[Dueue.top->data]);
+			if(i < num - 1)
+				printf(",");
+		}
+	}
+	printf("}\n");
+
+}
+
+int main(){
+	int buffer[1000];
+	int num;
+	int window;
+	while(scanf("%d", &num) != EOF){
+		int i;
+		for(i = 0; i < num; i++)
+			scanf("%d", buffer + i);
+		
+		scanf("%d", &window);
+
+		if(window > num){
+			printf("window too big\n");
+			continue;
+		}
+
+		getMaxWindows(num, buffer, window);
+	}
+	return 0;
+}
