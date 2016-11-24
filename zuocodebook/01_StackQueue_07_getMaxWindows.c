dd4d5721c72ebb0889aa91dc948d784d69fe55ec
Kevins Bobo <kevins.bobo@gmail.com>
2016-11-24 11:14:53 +0800

    edit 01_StackQueue_07_getMaxWindow.c

--- zuocodebook/01_StackQueue_07_getMaxWindows.c
+++ zuocodebook/01_StackQueue_07_getMaxWindows.c
@@ -96,6 +96,16 @@ Elemtype dueuePopTail(Dueuetop *Dueue){
 	return e;
 }
 
+/*
+* 技巧：
+* 1. 双队列队头存放当前窗口遇到的最大值下标
+* 2. 双队列队尾存放当前窗口新进的元素下标
+* 3. 当新进元素值大于等于队尾下标对应的元素时
+*    循环将队尾下标弹出，直到队列为空或新进元素值
+*    小于队尾下标对应的元素
+* 4. 当队头存放的下标等与每次循环中的 i - w 时，
+*    队头下标过期，将其弹出
+*/
 void getMaxWindows(int num, int const *buffer, int window){
 	Dueuetop Dueue;
 	init(&Dueue);
@@ -104,15 +114,26 @@ void getMaxWindows(int num, int const *buffer, int window){
 
 	printf("{");
 
-	for(i = 0;i < num; i ++){
+	for(i = 0; i < num; i ++){
+		/*
+		* 当前队列不为空并且当前窗口中未进行比较的元素大于等于
+		* 队列尾部下标所代表的元素时，
+		* 弹出队尾元素，循环至不满足以上条件
+		*/
 		while(Dueue.tail != NULL && buffer[Dueue.tail->data] <= buffer[i])
 			dueuePopTail(&Dueue);
-		
+
+		// 将当前元素下标存进队尾
 		dueuePushTail(&Dueue, i);
 
+		// 如果对头所存储的下标等于 i - window 时，下标过期，将其弹出
 		if(Dueue.top->data == i - window)
 			dueuePop(&Dueue);
 
+		/*
+		* 当走过第一个窗口的前 window - 1 个元素后，
+		* 每次循环都会打印队头元素，即当前窗口最大值。
+		*/
 		if(i >= window - 1){
 			printf("%d", buffer[Dueue.top->data]);
 			if(i < num - 1)
@@ -127,11 +148,14 @@ int main(){
 	int buffer[1000];
 	int num;
 	int window;
+	printf("input arr length:");
 	while(scanf("%d", &num) != EOF){
+		printf("input arr member:");
 		int i;
 		for(i = 0; i < num; i++)
 			scanf("%d", buffer + i);
-		
+
+		printf("input window length:");
 		scanf("%d", &window);
 
 		if(window > num){
@@ -140,6 +164,7 @@ int main(){
 		}
 
 		getMaxWindows(num, buffer, window);
+		printf("input arr length:");
 	}
 	return 0;
 }
